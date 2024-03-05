59909f87ae5498083f977fc8b0475d3dfc9b0142
ark <phutv1990@gmail.com>
2024-03-05 15:21:35 +0700

    build recommendation system part 2

--- src/main/java/com/ark/security/service/recommender/RecommenderService.java
+++ src/main/java/com/ark/security/service/recommender/RecommenderService.java
@@ -4,32 +4,32 @@ import com.ark.security.dto.ProductDto;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import com.ark.security.models.product.Product;
 import com.ark.security.models.recommender.TastePreferences;
-import com.ark.security.models.user.User;
 import com.ark.security.repository.OrderRepository;
 import com.ark.security.repository.recommender.TastePreferencesRepository;
 import com.ark.security.service.product.ProductService;
 import com.ark.security.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
 import org.apache.mahout.cf.taste.impl.model.jdbc.ReloadFromJDBCDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.*;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.model.JDBCDataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.distance.CosineDistanceMeasure;
+import org.apache.mahout.math.hadoop.similarity.cooccurrence.measures.CosineSimilarity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,12 +64,22 @@ public class RecommenderService {
 
     public List<ProductDto> recommendProductsForUser(int userId) throws TasteException {
         List<ProductDto> productDtos = new ArrayList<>();
-        migrateOrderDataToTastePreferences();
+//        migrateOrderDataToTastePreferences();
         try{
             DataModel dataModel = new ReloadFromJDBCDataModel(new MySQLJDBCDataModel(getDataSource()));
-            LogLikelihoodSimilarity similarity = new LogLikelihoodSimilarity(dataModel);
+//            LogLikelihoodSimilarity similarity = new LogLikelihoodSimilarity(dataModel);
+            SpearmanCorrelationSimilarity similarity = new SpearmanCorrelationSimilarity(dataModel);
+
+//            UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+//            UserSimilarity similarity = new EuclideanDistanceSimilarity(dataModel);
+//            ItemSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+//            ItemSimilarity similarity = new TanimotoCoefficientSimilarity(dataModel);
+//            UserSimilarity similarity = new TanimotoCoefficientSimilarity(dataModel);
             UserNeighborhood neighborhood = new NearestNUserNeighborhood(NEIGHBORHOOD_NUM, similarity, dataModel);
-            UserBasedRecommender recommender = new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+
+            UserBasedRecommender userBasedRecommender = new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+
+            Recommender recommender = new CachingRecommender(userBasedRecommender);
             List<RecommendedItem> recommendations = recommender.recommend(userId, 5);
             System.out.println("Recommneds for user " + userId + " are: ");
             System.out.println("**********************************************");
@@ -77,19 +87,19 @@ public class RecommenderService {
             for (RecommendedItem ri : recommendations){
                 int itemId = (int) ri.getItemID();
                 Product product = productService.getProductById(itemId);
-                float estimatePref = ri.getValue();
+                float estimatePref = recommender.estimatePreference(userId, itemId);
                 System.out.println(itemId + " " + product.getName() + "\t" + estimatePref);
                 ProductDto productDto = product.convertToDto();
                 productDtos.add(productDto);
             }
             System.out.println("**********************************************");
-            long[] userIds = recommender.mostSimilarUserIDs(userId, 5);
-            System.out.println("Most similar users to user " + userId + " are: ");
-            for (long uid : userIds){
-                int id = (int) uid;
-                User user = userService.getUserById(id);
-                System.out.println(user.getUsername());
-            }
+//            long[] userIds = recommender.mostSimilarUserIDs(userId, 5);
+//            System.out.println("Most similar users to user " + userId + " are: ");
+//            for (long uid : userIds){
+//                int id = (int) uid;
+//                User user = userService.getUserById(id);
+//                System.out.println(user.getUsername());
+//            }
         } catch (TasteException e){
             logger.error(String.valueOf(e));
         }
