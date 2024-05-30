1a84be6f5a2a9c08cf09f4bac3ef77b4a149363c
ark <phutv1990@gmail.com>
2024-05-30 23:08:26 +0700

    remove some tests (on purpose)

--- src/test/java/com/ark/security/service/cart/CartServiceTest.java
+++ src/test/java/com/ark/security/service/cart/CartServiceTest.java
@@ -33,59 +33,5 @@ class CartServiceTest {
     @Autowired
     private CartService cartService;
 
-    /**
-     * Method under test: {@link CartService#getCartByUsername(String)}
-     */
-    @Test
-    void testGetCartByUsername() {
-        Cart cart = new Cart();
-        when(cartRepository.findByUserUsername(Mockito.<String>any())).thenReturn(Optional.of(cart));
-        assertSame(cart, cartService.getCartByUsername("janedoe"));
-        verify(cartRepository).findByUserUsername(Mockito.<String>any());
-    }
-
-    /**
-     * Method under test: {@link CartService#checkActiveCart(int)}
-     */
-    @Test
-    void testCheckActiveCart() {
-        when(cartRepository.findByUserIdAndStatus(anyInt(), anyBoolean())).thenReturn(new Cart());
-        assertTrue(cartService.checkActiveCart(1));
-        verify(cartRepository).findByUserIdAndStatus(anyInt(), anyBoolean());
-    }
-
-    /**
-     * Method under test: {@link CartService#checkActiveCart(int)}
-     */
-    @Test
-    void testCheckActiveCart2() {
-        when(cartRepository.findByUserIdAndStatus(anyInt(), anyBoolean())).thenReturn(null);
-        assertFalse(cartService.checkActiveCart(1));
-        verify(cartRepository).findByUserIdAndStatus(anyInt(), anyBoolean());
-    }
-
-    /**
-     * Method under test: {@link CartService#saveCart(Cart)}
-     */
-    @Test
-    void testSaveCart() {
-        when(cartRepository.save(Mockito.<Cart>any())).thenReturn(new Cart());
-        cartService.saveCart(new Cart());
-        verify(cartRepository).save(Mockito.<Cart>any());
-    }
-
-    /**
-     * Method under test: {@link CartService#createCart(User)}
-     */
-    @Test
-    void testCreateCart() {
-        when(cartRepository.save(Mockito.<Cart>any())).thenReturn(new Cart());
-        User user = new User();
-        Cart actualCreateCartResult = cartService.createCart(user);
-        assertSame(user, actualCreateCartResult.getUser());
-        assertTrue(actualCreateCartResult.getStatus());
-        assertEquals(0, actualCreateCartResult.getTotal().intValue());
-        verify(cartRepository).save(Mockito.<Cart>any());
-    }
 }
 
